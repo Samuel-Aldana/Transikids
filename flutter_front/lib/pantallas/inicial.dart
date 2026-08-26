5debedcba591bd2eae58a3fd8eed0337d860516d
Samuel Aldana <samueljosue21garcia@gmail.com>
2026-08-26 15:17:37 -0500

    Replace EntradaPrincipal with Datos in Inicial screen

--- flutter_front/lib/pantallas/inicial.dart
+++ flutter_front/lib/pantallas/inicial.dart
@@ -4,7 +4,7 @@ import 'package:flutter_application_1/bases/botones/olvido.dart';
 import 'package:flutter_application_1/bases/botones/registro.dart';
 import 'package:flutter_application_1/bases/imagenes/ciudad.dart';
 import 'package:flutter_application_1/bases/imagenes/logo.dart';
-import 'package:flutter_application_1/pantallas/entrada_principal.dart';
+import 'package:flutter_application_1/pantallas/datos.dart';
 
 class Inicial extends StatefulWidget {
   const Inicial({super.key});
@@ -18,30 +18,38 @@ class _InicialState extends State<Inicial> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: 60),
-        Logo(),
-        Ciudad(),
-        SizedBox(height: 90),
-        //boton temporal de nav hacia el apartado de tracking
-        ElevatedButton(
-          onPressed: () {
+        const SizedBox(height: 60),
+        const Logo(),
+        const Ciudad(),
+        const SizedBox(height: 90),
+
+        Iniciars(
+          texto: "Iniciar sesión",
+          funcion: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) /*este bicho es un return*/ =>
-                    const EntradaPrincipal(),
+                builder: (context) => const Datos(),
               ),
             );
           },
-          child: const Text('Pocoyo'),
         ),
-        //uwu
-        Iniciars(),
-        SizedBox(height: 20),
-        Registro(),
-        SizedBox(height: 10),
-        Recuperar(),
-        Spacer(),
+
+        const SizedBox(height: 20),
+
+        Registro(
+          texto: "registrarse",
+          funcion: () {},
+        ),
+
+        const SizedBox(height: 10),
+
+        Recuperar(
+          texto: "¿olvidaste tu contraseña?",
+          funcion: () {},
+        ),
+
+        const Spacer(),
       ],
     );
   }
